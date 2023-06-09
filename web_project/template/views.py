5d566b3f0af4598c9426d770314bb531bf6dab8d
tmgross <tmgross8@gmail.com>
2023-06-09 14:37:13 -0400

    Added template for pages

--- web_project/template/views.py
+++ web_project/template/views.py
@@ -1,4 +1,22 @@
 from django.http import HttpResponse
 
+#replace return HttpResponse
+# return render(request, 'homepage/homepage.html')
+
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def aboutUs(request):
+    return HttpRespone("Placeholder")
+
+def outreach(request):
+    return HttpRespone("Placeholder")
+
+def demos(request):
+    return HttpRespone("Placeholder")
+
+def schedule(request):
+    return HttpRespone("Placeholder")
+
+def resources(request):
+    return HttpRespone("Placeholder")
