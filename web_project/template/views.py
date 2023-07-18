d7c41dc628bd2fde5e1df75a82307c9c2d1f1acf
sonickels <96350461+sonickels@users.noreply.github.com>
2023-07-18 13:37:59 -0400

    Schedule draft
    Schedule page is still not up and running, but this is to save a portion of it

--- web_project/template/views.py
+++ web_project/template/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
-#replace return HttpResponse
-# return render(request, 'homepage/homepage.html')
+from django.views import generic
+from django.utils.safestring import mark_safe
+from datetime import datetime
+from .models import *
+from .utils import Calendar
 
 def home(request):
     return render(request, 'homepage.html')
@@ -21,3 +23,24 @@ def schedule(request):
 
 def resources(request):
     return render(request, 'resources.html')
+
+
+class CalendarView(generic.ListView):
+    model = Event
+    template_name = 'schedule.html'
+
+    def get_context_data(self, **keyword_args):
+        context = super.get_context_data(**keyword_args)
+        d = get_date(self.request.GET.get('day', None))
+        cal = Calendar(d.year, d.month)
+
+        # Call the formatmonth method, which returns our calendar as a table
+        html_cal = cal.formatmonth(withyear=True)
+        context['calendar'] = mark_safe(html_cal)
+        return context
+
+def get_date(req_day):
+    if req_day:
+        year, month = (int(x) for x in req_day.split('-'))
+        return date(year, month, day=1)
+    return datetime.today()
